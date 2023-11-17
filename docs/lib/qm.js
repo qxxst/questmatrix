61642ff8f72e8f4cc9035469c364958b1341257f
questmatrix <31394614+questmatrix@users.noreply.github.com>
2023-11-16 23:13:21 -0600

    Update qm.js

--- docs/lib/qm.js
+++ docs/lib/qm.js
@@ -1,1 +1,7 @@
-//placeholder
+// qm.js by Quest Matrix
+function process() {
+    var images = document.querySelectorAll('img:not([alt])');
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.border = '5px solid red';
+    }
+}
